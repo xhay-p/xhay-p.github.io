09b305d6265224a8237b63a83be18084a0311aaf
xhay-p <sendtoakshayparekh@gmail.com>
2026-05-03 17:52:06 +0530

    added hfprofile

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,5 +55,49 @@ jQuery(document).ready(function($) {
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
 
+    /* Fetch last commit dates for tutorials */
+    function fetchLastCommitDate(repo, path, element) {
+        var apiUrl = 'https://api.github.com/repos/' + repo + '/commits';
+        if (path) {
+            apiUrl += '?path=' + encodeURIComponent(path) + '&per_page=1';
+        } else {
+            apiUrl += '?per_page=1';
+        }
+        
+        $.ajax({
+            url: apiUrl,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                if (data && data.length > 0 && data[0].commit && data[0].commit.author && data[0].commit.author.date) {
+                    var commitDate = new Date(data[0].commit.author.date);
+                    var formattedDate = commitDate.toLocaleDateString('en-US', { 
+                        year: 'numeric', 
+                        month: 'short', 
+                        day: 'numeric' 
+                    });
+                    element.find('.update-date').text(formattedDate);
+                    element.show();
+                }
+            },
+            error: function(xhr, status, error) {
+                // Silently fail if API call fails (e.g., rate limit)
+                console.log('Could not fetch commit date for ' + repo);
+            }
+        });
+    }
+
+    // Fetch commit dates for all tutorial items
+    $('.item[data-repo]').each(function() {
+        var $item = $(this);
+        var repo = $item.data('repo');
+        var path = $item.data('path');
+        var $lastUpdated = $item.find('.last-updated');
+        
+        if (repo && $lastUpdated.length) {
+            fetchLastCommitDate(repo, path, $lastUpdated);
+        }
+    });
+
 
 });
